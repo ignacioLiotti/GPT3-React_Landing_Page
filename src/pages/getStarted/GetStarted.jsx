af7cbd37ce798b313480b9b5d719726b114c8389
IgnacioLiotti <ignacioliotti@gmail.com>
2022-04-28 13:56:57 -0300

    Added proper Sign Up page

--- src/pages/getStarted/GetStarted.jsx
+++ src/pages/getStarted/GetStarted.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './signUp.css'
 
 const getStarted = () => {
   return (
-    <div>getStarted</div>
+    <div>
+      <h1>Let's get started!</h1>
+      <h2>Create an account</h2>
+      <p>Complete all the fields to have your new GPT 3 interactive account</p>
+      <form>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <label>
+          Email:
+          <input type="email" />
+        </label>
+        <label>
+          Yes, I agree to the Terms of Service and Privacy Policy
+          <input type="checkbox" />
+        </label>
+        <label>
+          I want to receive emails from GPT 3 newsletter
+          <input type="checkbox" />
+        </label>
+        <p>By creating an account, you agree to our Terms of Service and have reviewed our Privacy Policy. This site is protected by nothing at all and is just a project for fun. Nothing you write here is going to be sent anywere. Fill in everything, or not, nothing is going to happen.</p>
+        <button>Create account</button>
+      </form>
+      <a href="">Have an account? LOGIN</a>
+    </div>
   )
 }
 
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import './signUp.css'
 
-const signUp = () => {
+const SignUp = () => {
   return (
-    <div>signUp</div>
+    <div className='gpt3__signUp section__padding gradient__bg'>
+      <div className='gpt3__signUp-container'>
+        <h1 className='gradient__text'>Let's get started!</h1>
+        <h2>Create an account</h2>
+        <p>Complete all the fields to have your new GPT 3 interactive account</p>
+      <form>
+        <div className='input-box'>
+          <input type="text" placeholder=' '/>
+          <label>Username:</label>
+        </div>
+        <div className='input-box'>
+          <input type="password" placeholder=' '/>
+          <label>Password:</label>
+        </div>
+        <div className='input-box'>
+          <input type="email" placeholder=' ' />
+          <label>Email:</label>
+        </div>
+        <div>
+        <label className='input-check'>
+          <input type="checkbox" />
+          Yes, I agree to the Terms of Service and Privacy Policy
+        </label>
+        </div>
+        <div>
+        <label className='input-check'>
+          <input type="checkbox" />
+          I want to receive emails from GPT 3 newsletter
+        </label>
+        </div>
+        <p>By creating an account, you agree to our Terms of Service and have reviewed our Privacy Policy. This site is protected by nothing at all and is just a project for fun. Nothing you write here is going to be sent anywere. Fill in everything, or not, nothing is going to happen.</p>
+        <div className='gpt3__signUp-btn'>
+          <button>Create account</button>
+        </div>
+      </form>
+      <a href="">Have an account? LOGIN</a>
+      </div>
+    </div>
   )
 }
 
-export default signUp
+export default SignUp
